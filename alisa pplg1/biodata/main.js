bc10d5789551c006eef554d6084971b10e6eb9ea
Alisafadilah <141700589+Alisafadilah@users.noreply.github.com>
2023-12-13 09:48:39 +0700

    Add files via upload

---
+++ alisa pplg1/biodata/main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Alisa Rahma";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 16";
+let alamat = "Alamat: Kp.cimerak";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
